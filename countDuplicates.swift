aa550c70652734e44ebd767631f76bb014266ce1
Andy Bryan Julca Tello <julcac137@gmail.com>
2024-01-12 11:45:14 -0500

    first commit

---
+++ countDuplicates.swift
@@ -0,0 +1,10 @@
+func countDuplicates(_ s:String) -> Int {
+    var counts: [String: Int] = [:]
+    for character in Array(s) {
+        counts[character.lowercased(), default: 0] +=  1
+      }
+     return counts.values.filter{ $0 > 1 }.count
+}
+
+print(countDuplicates("eerrttyy"))
+
